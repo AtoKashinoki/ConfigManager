648a3f6f0aab6017a7aeb2ef687889cd2c062424
AtoKashinoki <atokashinoki@gmail.com>
2024-05-06 10:15:34 +0900

    Create base process of __init__.py

---
+++ src/ConfigManager/__init__.py
@@ -0,0 +1,20 @@
+"""
+    Config Manager
+
+This module introduces config management classes and functions.
+
+"""
+
+# import sys
+import sys
+
+
+__self_name__ = "ConfigManager"
+
+
+if not __name__ == __self_name__:
+    print("execute failed")
+    sys.exit()
+
+
+print("Initialize {}.".format(__self_name__))
