9d11635dc974ba63cb4823d781eb170eb211e5a5
AtoKashinoki <atokashinoki@gmail.com>
2024-05-06 10:18:17 +0900

    Create main.py file

---
+++ src/ConfigManager/main.py
@@ -0,0 +1,12 @@
+"""
+    ConfigManager.main
+
+This file contain Config class.
+That manage config file and config datas.
+
+"""
+
+# import descriptors
+from Descriptors import (
+    DataType,
+)
